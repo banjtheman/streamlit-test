df93c810836549e7f45fa36e826b32e156a0622d
Banjo <banjtheman@gmail.com>
2020-09-16 18:07:39 -0400

    adding dataframe

--- test.py
+++ test.py
@@ -8,9 +8,21 @@ import pandas as pd
 import streamlit as st
 
 
+@st.cache
+def load_external_data():
+    sample_iris_url = "https://raw.githubusercontent.com/aether-ai/grimoire/master/grim/sample_data/iris.csv"
+    df = pd.read_csv(sample_iris_url)
+    return df
+
+
 
 def main():
-    st.write("Hello from teams")
+    st.title("Teams Beta")
+    st.header("Hello from teams")
+
+    st.write("Here is a dataframe")
+    df = load_external_data()
+    st.dataframe(df)
 
 
 
